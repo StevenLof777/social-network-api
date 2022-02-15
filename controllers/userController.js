ec88024d4576b5491177f5c74ca68d8d7ce9af0a
Jake Finn <lof.steven@gmail.com>
2022-02-15 15:50:17 -0600

    Fix update user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,6 @@
 const User = require('../models/User')
 
 const getUsers = (req, res) => {
-    console.log('hit em up 2pac')
     User.find({})
     .then((users) => res.json(users))
     .catch(err => {
@@ -19,9 +18,8 @@ const createUser = (req, res) => {
         return res.status(500).json(err);
     })
 }
-// Doesn't Work
+
 const getSingleUser = (req, res) => {
-    console.log('hit em up 2pac')
     User.findOne({ _id: req.params.userId })
     .then((user) => res.json(user))
     .catch(err => {
@@ -31,7 +29,11 @@ const getSingleUser = (req, res) => {
 }
 // Doesn't work
 const updateUser = (req, res) => {
-    User.findOneAndUpdate({ _id: req.params.userId })
+    User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $set: req.body },
+        { runValidators: true, new: true }
+        )
     .then((users) => res.json(users))
     .catch(err => {
         console.log(err)
@@ -39,13 +41,14 @@ const updateUser = (req, res) => {
     })
 }
 // Doesn't work
-const deleteUser = (req, res) => {
-    User.findOneAndDelete({ _id: req.params.userId })
-    .then((users) => res.json(users))
-    .catch(err => {
-        console.log(err)
-        return res.status(500).json(err);
-    })
-}
-
-module.exports = { createUser, getUsers, getSingleUser, updateUser, deleteUser }
+// const deleteUser = (req, res) => {
+//     User.findOneAndDelete({ _id: req.params.userId })
+//     .then((users) => res.json(users))
+//     .catch(err => {
+//         console.log(err)
+//         return res.status(500).json(err);
+//     })
+// }
+module.exports = { createUser, getUsers, getSingleUser, updateUser,
+    //  deleteUser
+     }
--- routes/thoughtsRoutes.js
+++ routes/thoughtsRoutes.js
@@ -4,7 +4,7 @@ const {
   getSingleThought,
   createThought,
   updateThought,
-  deleteThought
+  // deleteThought
 } = require('../../controllers/studentController');
 
 
@@ -12,10 +12,11 @@ router.route('/api/users').get(getThoughts).post(createThought);
 
 router.route('api/users/:userId').get(getSingleThought).put(updateThought);
 
-router.route('api/users/:userId').get(getSingleThought).delete(deleteThought);
+router.route('api/users/:userId').get(getSingleThought)
+// .delete(deleteThought);
 
-router.route('/api/users/:userId/friends/').post();
+// router.route('/api/users/:userId/friends/').post();
 
-router.route('/api/users/:userId/friends/:friendId').delete();
+// router.route('/api/users/:userId/friends/:friendId').delete();
 
 module.exports = router;
